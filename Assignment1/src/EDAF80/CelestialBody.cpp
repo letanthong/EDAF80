15f1a123ba6e0aa6050e72f534df37b4cfa08c8b
Thong Le <tle@borgwarner.com>
2023-09-13 20:26:33 +0200

    Excercise 4 - Assignment 1

--- Assignment1/src/EDAF80/CelestialBody.cpp
+++ Assignment1/src/EDAF80/CelestialBody.cpp
@@ -26,48 +26,42 @@ glm::mat4 CelestialBody::render(std::chrono::microseconds elapsed_time,
 	// milliseconds, the following would have been used:
 	//auto const elapsed_time_ms = std::chrono::duration<float, std::milli>(elapsed_time).count();
 
-	_body.spin.rotation_angle = _body.spin.rotation_angle + (elapsed_time_s * _body.spin.speed); //Ex 2.6
+	_body.spin.rotation_angle += elapsed_time_s * _body.spin.speed; //Ex 2.6
 
 	glm::mat4 world = parent_transform;
-
-	//Excercise 1
-	glm::mat4 I = glm::mat4(1.0f);
-	//glm::vec3 V = glm::vec3(1.0f, 0.2f, 0.2f); //Scale factor - Flat //Ex 1.4
-	glm::vec3 V = glm::vec3(1.0f); //Scale factor /7Ex 1.3
-	glm::mat4 S = glm::scale(I, V);
-	//world = S;
-	//End excercise 1
-	//Self-note
-	// Blue - x-axis
-	// Red - y-axis
-	// Green - z-axis
-
-	//Excercise 2
-	glm::vec3 earth_centergrav = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 children_transform(1.0f);
 	glm::vec3 X_axis = glm::vec3(1.0f, 0.0f, 0.0f);
 	glm::vec3 Y_axis = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::vec3 Z_axis = glm::vec3(0.0f,0.0f,1.0f);
+	glm::vec3 Z_axis = glm::vec3(0.0f, 0.0f, 1.0f);
+	glm::mat4 I = glm::mat4(1.0f);
+	glm::mat4 child_transform = glm::mat4(1.0f);
+	int iNbrChild = 0;
+
+	//Excercise 1
+	//glm::vec3 V = glm::vec3(1.0f, 0.2f, 0.2f); //Scale factor - Flat //Ex 1.4
+	//glm::vec3 V = glm::vec3(1.0f); //Scale factor /7Ex 1.3
+	glm::vec3 scale = _body.scale;
+	glm::mat4 S = glm::scale(I, scale);
+	//world = S;
+	//End excercise 1
+
+	//Excercise 2
 	glm::mat4 R1s = glm::rotate(I, _body.spin.rotation_angle, Y_axis); //Ex 2.1
 	glm::mat4 R2s = glm::rotate(I, _body.spin.axial_tilt, Z_axis); //Ex 2.2
 	//world = R1s; //Ex 2.3, 2.4, 2.5
 	//End excercise 2
 
 	//Excercise 3
-	_body.orbit.rotation_angle = _body.orbit.rotation_angle + (elapsed_time_s * _body.orbit.speed); //Ex 3.3
+	_body.orbit.rotation_angle += elapsed_time_s * _body.orbit.speed; //Ex 3.3
 	glm::vec3 t = glm::vec3( _body.orbit.radius, 0.0f, 0.0f); //How to access the earth_orbit variable?
-	glm::mat4 To = glm::translate(I, t);
+
+	glm::mat4 To = glm::translate(I, t); //Translate the orbit center
+	//glm::mat4 dummy = glm::translate(I, glm::vec3(-2.5f, 0.0f, 0.0f)); //Translate the orbit center
 	glm::mat4 R1o = glm::rotate(I, _body.orbit.rotation_angle, Y_axis); //Rotate around the orbit center
 	glm::mat4 R2o = glm::rotate(I, _body.orbit.inclination, Z_axis); //Ex 3.5 - Tilt the orbit plane to the z-axis
-	world = R1o*To*S; //Ex 3.1, 3.2, 3.4 , 3.6, 4.1
+	world = parent_transform * R2o * R1o * To * R2s * R1s * S ; //Ex 3.1, 3.2, 3.4 , 3.6, 4.1
+	//world = R2s * R1s * S * parent_transform; //Ex 3.1, 3.2, 3.4 , 3.6, 4.1
 	//End excercise 3
-
-	//Excercise 4: Calculate child's transformation
-	glm::mat4 child_transform(1.0f);
-	//Translate the child's center to the parent's center
-
-	glm::mat4 t_c = glm::translate(I, glm::vec3(_body.orbit.radius, 0.0f, 0.0f));
-	child_transform = world;
-	//End excercise 4
 
 	if (show_basis)
 	{
@@ -80,10 +74,13 @@ glm::mat4 CelestialBody::render(std::chrono::microseconds elapsed_time,
 	// manage all the local transforms ourselves, so the internal transform
 	// of the node is just the identity matrix and we can forward the whole
 	// world matrix.
+
 	_body.node.render(view_projection, world);
 
-	//return parent_transform; //Original code
-	return child_transform;
+	//Excercise 4: Calculate child's transformation
+	children_transform = R2o * R1o *  To; //Apply the parent translation to the orbit and the orbit rotation + inclination
+	//End excercise 4
+	return children_transform;
 }
 
 void CelestialBody::add_child(CelestialBody* child)
