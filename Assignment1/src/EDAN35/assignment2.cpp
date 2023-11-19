26b3c59731d202460319e61131d8ad853202af5a
Thong Le <tle@borgwarner.com>
2023-11-19 21:16:03 +0100

    Lab 2 1.4 Clear color buffer and write normal to geometry normal

--- Assignment1/src/EDAN35/assignment2.cpp
+++ Assignment1/src/EDAN35/assignment2.cpp
@@ -463,6 +463,7 @@ edan35::Assignment2::run()
 			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbos[toU(FBO::GBuffer)]);
 			glViewport(0, 0, framebuffer_width, framebuffer_height);
 			glClear(GL_DEPTH_BUFFER_BIT);
+			glClear(GL_COLOR_BUFFER_BIT); //tle Lab2_1.4 _ Because the color buffer was not reset
 			// XXX: Is any other clearing needed?
 
 			glUseProgram(fill_gbuffer_shader);
